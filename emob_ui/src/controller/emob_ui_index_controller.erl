e9f18db93e32620ad1d300373824f37f796534bd
Tom Heinan <me@tomheinan.com>
2012-07-07 16:29:33 -0400

    Added an authentication guard to the index controller.

--- emob_ui/src/controller/emob_ui_index_controller.erl
+++ emob_ui/src/controller/emob_ui_index_controller.erl
@@ -1,8 +1,21 @@
 -module(emob_ui_index_controller, [Req, SessionID]).
--export([landing_page/2, home/2]).
+-export([before_/1, landing_page/3, home/3]).
 
-landing_page('GET', []) ->
-	{ok, []}.
+before_(_) ->
+	AccessToken = boss_session:get_session_data(SessionID, access_token),
+	if
+		AccessToken == "undefined" ->
+			false;
+		true ->
+			true
+	end.
 
-home('GET', []) ->
-	{ok, [{session_id, SessionID}]}.
+landing_page('GET', [], false) ->
+	{ok, []};
+landing_page('GET', [], true) ->
+	{redirect, [{action, "home"}]}.
+
+home('GET', [], true) ->
+	{ok, [{session_id, SessionID}]};
+home('GET', [], false) ->
+	{redirect, [{action, "landing_page"}]}.
