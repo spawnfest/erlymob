b78ee25808b770b5925b5e13e8e6d0af06ea7639
Tom Heinan <me@tomheinan.com>
2012-07-08 15:29:47 -0400

    Fixing get_request_token.

--- emob_ui/src/lib/emob_transport.erl
+++ emob_ui/src/lib/emob_transport.erl
@@ -2,9 +2,9 @@
 -export([get_request_token/0, get_access_token/2]).
 
 get_request_token() ->
-	{_, ApiUrl} = application:get_env(emob_ui, api_root),
+	{ok, ApiUrl} = application:get_env(emob_ui, api_root),
 	TargetUrl = io_lib:format("~s/get_request_token", [ApiUrl]),
-	{_, CBUrl} = application:get_env(emob_ui, local_root),
+	{ok, CBUrl} = application:get_env(emob_ui, local_root),
 	GetData = io_lib:format("callback_url=~s/post_login", [CBUrl]),
 	case ibrowse:send_req(TargetUrl, [{"Accept", "application/json"}], get, [GetData], [{response_format, binary}]) of
 		{ok, Code, Headers, Body} ->
@@ -25,7 +25,7 @@ get_request_token() ->
 	end.
 
 get_access_token(OAuthToken, OAuthVerifier) ->
-	{_, ApiUrl} = application:get_env(emob_ui, api_root),
+	{ok, ApiUrl} = application:get_env(emob_ui, api_root),
 	TargetUrl = io_lib:format("~s/get_access_token?oauth_token=~s&oauth_verifier=~s", [ApiUrl, OAuthToken, OAuthVerifier]),
 	case ibrowse:send_req(TargetUrl, [{"Accept", "application/json"}], get, [], [{response_format, binary}]) of
 		{ok, Code, Headers, Body} ->
