fe49f938e2d52ccaa74dbed19b7ebc721f59826c
Tom Heinan <me@tomheinan.com>
2012-07-08 19:03:35 -0400

    Updated Mahesh's blerb.

--- emob_ui/src/controller/emob_ui_info_controller.erl
+++ emob_ui/src/controller/emob_ui_info_controller.erl
@@ -1,8 +1,10 @@
--module(emob_ui_info_controller, [Req]).
+-module(emob_ui_info_controller, [Req, SessionID]).
 -export([about/2, team/2]).
 
 about('GET', []) ->
-	{ok, []}.
+	{ok, LoggedIn} = emob_ui_user:check_auth(SessionID),
+	{ok, [{logged_in, LoggedIn}]}.
 
 team('GET', []) ->
-	{ok, []}.
+	{ok, LoggedIn} = emob_ui_user:check_auth(SessionID),
+	{ok, [{logged_in, LoggedIn}]}.
