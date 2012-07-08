3e1b3ec53bb57208fa933c94490287e54fe3c000
Tom Heinan <me@tomheinan.com>
2012-07-08 13:52:00 -0400

    Emob transport fixes.

--- emob_ui/src/controller/emob_ui_authentication_controller.erl
+++ emob_ui/src/controller/emob_ui_authentication_controller.erl
@@ -2,5 +2,9 @@
 -export([login/2]).
 
 login('GET', []) ->
-	RequestToken = emob_transport:get_request_token(),
-	{ok, []}.
+	RequestBody = emob_transport:get_request_token({callback_url, "http://localhost:3000/post_login"}),
+	Json = element(1, RequestBody),
+	RequestToken = kvc:path(result.token, Json),
+	Url = "https://api.twitter.com/oauth/authenticate?oauth_token=" ++ binary_to_list(RequestToken),
+	io:format("~p~n", [Url]),
+	{redirect, [Url]}.
--- emob_ui/src/lib/emob_transport.erl
+++ emob_ui/src/lib/emob_transport.erl
@@ -1,14 +1,17 @@
 -module(emob_transport).
--export([get_request_token/0]).
+-export([get_request_token/1]).
 
-get_request_token() ->
-	case ibrowse:send_req("http://localhost:8080/get_request_token", [{"Accept", "application/json"}], get, ["callback_url=http://localhost:8080/get_access_token"], [{response_format, binary}]) of
+get_request_token(CallbackUrl) ->
+	{_, Url} = CallbackUrl,
+	Body = "callback_url=" ++ Url,
+	case ibrowse:send_req("http://localhost:8080/get_request_token", [{"Accept", "application/json"}], get, [Body], [{response_format, binary}]) of
 		{ok, Code, Headers, Body} ->
 			try
 				case Code of
 					%% Only 2xx HTTP response codes are considered successful (is this correct?)
 					"2" ++ _Tail ->
-						{ok, Body};
+						JsonData = ejson:decode(Body),
+						JsonData;
 					_  ->
 						%% In case of errors, return the reason corresponding to the HTTP response code and
 						%% the error document returned by MLAPI.
