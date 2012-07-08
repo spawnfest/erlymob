82c3699775bd4f5af6bd3ebf7776dfdb55c532c3
Tom Heinan <me@tomheinan.com>
2012-07-08 16:23:09 -0400

    Added user and session metadata to /home.

--- emob_ui/src/controller/emob_ui_index_controller.erl
+++ emob_ui/src/controller/emob_ui_index_controller.erl
@@ -13,4 +13,14 @@ landing_page('GET', [], false) ->
 home('GET', [], false) ->
 	{redirect, [{controller, "index"}, {action, "landing_page"}]};
 home('GET', [], true) ->
-	{ok, [{logged_in, true}]}.
+	AccessToken = boss_session:get_session_data(SessionID, access_token),
+	AccessTokenSecret = boss_session:get_session_data(SessionID, access_token_secret),
+	UserId = boss_session:get_session_data(SessionID, user_id),
+	ScreenName = boss_session:get_session_data(SessionID, screen_name),
+	{ok,[
+		{logged_in, true},
+		{session_id, SessionID},
+		{access_token, AccessToken},
+		{access_token_secret, AccessTokenSecret},
+		{user_id, UserId},
+		{screen_name, ScreenName}]}.
